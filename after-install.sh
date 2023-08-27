51358a0b78a20cb747dd5dea9ec302ecd33af545
Binaya Baral <binaya.baral5@gmail.com>
2023-08-27 21:57:13 +0545

    feat: run migration on codedeploy completion

--- after-install.sh
+++ after-install.sh
@@ -8,5 +8,6 @@ cd ~/source/codedeploy/backend && yarn
 # Letting pm2 know the homepath otherwise codeDeploy was not able to execute the
 # pm2 commands
 source /home/ubuntu/.profile
+yarn migrate
 PM2_HOME=/home/ubuntu/.pm2 pm2 startOrReload pm2.config.js
 PM2_HOME=/home/ubuntu/.pm2 pm2 reload backend --update-env
